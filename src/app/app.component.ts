e65e09b785e9edd97efe13078bbe84af53b2e86b
Lucas Hinegardner <lucas.hinegardner@gmail.com>
2017-12-18 12:29:55 -0500

    initial angular example

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core'; //at sign shows root folder for angular
+
+@Component({
+  selector: 'app-root', //you make up the html tag that you wanna use
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'] //an array of files(string path), can add more files
+})
+export class AppComponent { //we will always export our class 
+  title: string = 'java'; //changes the title on webpage, will reload automatically
+  //could type this as a string
+}
